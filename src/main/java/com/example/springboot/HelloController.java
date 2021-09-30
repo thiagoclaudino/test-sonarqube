95021844dd3ab03bbe1bcd823ba1861dcbd42252
Thiago Claudino <88042055+thiagoclaudino@users.noreply.github.com>
2021-09-30 10:25:14 -0300

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
-	private String password = "aske3#%nekeejKz"
+	private String password = "aske3#%nekeejKz";
 
 	@GetMapping("/")
 	public String index() {
